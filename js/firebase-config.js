44c7e093ec7015b265a532b17ca1c636b166b090
AIHEALTHYMARKET <diabolo.weien@gmail.com>
2026-09-21 23:22:06 +0800

    Update and rename config.js to js/firebase-config.js

---
+++ js/firebase-config.js
@@ -0,0 +1,29 @@
+// ============================================================
+// BS GLOBAL Recognition System
+// Firebase 設定
+// ============================================================
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyDpWeR78Lwfi9mDOS4mCzUsTYVI7SOsDVY",
+  authDomain: "bsrecog.firebaseapp.com",
+  projectId: "bsrecog",
+  storageBucket: "bsrecog.firebasestorage.app",
+  messagingSenderId: "104751827465",
+  appId: "1:104751827465:web:ee5e360dedbd4a70b246d8",
+  measurementId: "G-JEPWHQ6FQ9"
+};
+
+// ============================================================
+// Google Apps Script Web App
+// ⚠️ 把下面這個網址換成你自己的 /exec 網址
+// ============================================================
+
+export const APPS_SCRIPT_URL =
+  "請把你的Google Apps Script /exec網址貼在這裡";
+
+// ============================================================
+// 管理員 Google Email
+// ============================================================
+
+export const ADMIN_EMAIL =
+  "diabolo.weien@gmail.com";
