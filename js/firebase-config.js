853a43dcf366f3f44b8fbc9802a3f9de35eccef8
AIHEALTHYMARKET <diabolo.weien@gmail.com>
2026-09-22 22:32:36 +0800

    Refactor firebase-config to use named exports

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,50 +1,29 @@
 // ============================================================
 // BS GLOBAL Recognition System
-// Firebase Configuration
+// Firebase 設定
 // ============================================================
 
-const firebaseConfig = {
-    apiKey: "AIzaSyDpWeR78Lwfi9mDOS4mCzUsTYVI7SOsDV",
-    authDomain: "bsrecog.firebaseapp.com",
-    projectId: "bsrecog",
-    storageBucket: "bsrecog.firebasestorage.app",
-    messagingSenderId: "104751827465",
-    appId: "1:104751827465:web:ee5e360dedbd4a70b246d8",
-    measurementId: "G-JEPWHQ6FQ9"
+export const firebaseConfig = {
+  apiKey: "AIzaSyDpWeR78Lwfi9mDOS4mCzUsTYVI7SOsDVY",
+  authDomain: "bsrecog.firebaseapp.com",
+  projectId: "bsrecog",
+  storageBucket: "bsrecog.firebasestorage.app",
+  messagingSenderId: "104751827465",
+  appId: "1:104751827465:web:ee5e360dedbd4a70b246d8",
+  measurementId: "G-JEPWHQ6FQ9"
 };
-
 
 // ============================================================
 // Google Apps Script Web App
+// ⚠️ 把下面這個網址換成你自己的 /exec 網址
 // ============================================================
 
-const APPS_SCRIPT_URL =
-    "https://script.google.com/macros/s/AKfycbycfMlsXP84tZxRB22oDpoAEStBqvi_dt3zhVsEzNJ9YKL75XEKIca-ZEl8weMl_iqe/exec";
-
-
-// ============================================================
-// Administrator
-// ============================================================
-
-const ADMIN_EMAIL =
-    "diabolo.weien@gmail.com";
-
+export const APPS_SCRIPT_URL =
+  "https://script.google.com/macros/s/AKfycbycfMlsXP84tZxRB22oDpoAEStBqvi_dt3zhVsEzNJ9YKL75XEKIca-ZEl8weMl_iqe/exec";
 
 // ============================================================
-// Google Drive Photo Folder
+// 管理員 Google Email
 // ============================================================
 
-const PHOTO_FOLDER_ID =
-    "1TmPzlU-txD8klUzoOf_rLIMUNNpp7Ejv";
-
-
-// ============================================================
-// Export
-// ============================================================
-
-export {
-    firebaseConfig,
-    APPS_SCRIPT_URL,
-    ADMIN_EMAIL,
-    PHOTO_FOLDER_ID
-};
+export const ADMIN_EMAIL =
+  "diabolo.weien@gmail.com";
